80b9bdcc2bb240e405bde4e6e45a979b4ce27c4a
Jonathan Dolan <yonidolan1@gmail.com>
2024-05-30 17:00:09 +0300

    Init repo

---
+++ js/services/book.service.js
@@ -0,0 +1,28 @@
+'use strict'
+
+'use strict'
+
+var books = [
+    {
+        id: bg4J78,
+        title: 'The adventures of Lori Ipsi',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: kj8545,
+        title: 'World Atlas',
+        price: 300,
+        imgUrl: 'world-atlas.jpg'
+    },
+    {
+        id: kf3586,
+        title: 'Zorba the Greek',
+        price: 87,
+        imgUrl: 'zorba.jpg'
+    }
+]
+
+function onInit(){
+    render()
+}
